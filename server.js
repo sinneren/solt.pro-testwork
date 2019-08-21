119922cf2a913329250ee74c97e65935bf6a04fa
slaskin <clickfastdev@gmail.com>
2019-08-21 23:58:38 +0300

    yet another server changes

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
-var express = require('express');
-var path = require('path');
+'use strict';
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-var app = express();
+const path = require('path');
+const app = require('express')();
 
-app.use(express.static(path.join(__dirname, 'build')));
+const port = process.env.PORT || 8080;
 
-app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, 'build/index.html'));
-});
+app.use('/', express.static(path.join(__dirname, 'build/index.html')));
 
-app.listen(server_port, server_host, (err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Listening on port %d', server_port);
-    }
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}/`);
 });
